d68cdadeafb4a03ddb684c4d593ec86e30ba9af0
Shannarrra <petarangelov15@gmail.com>
2020-04-28 17:48:21 +0300

    initial commit

---
+++ Graph.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GRAPHics
+{
+    /// <summary>
+    /// A simple, undirected graph presented through <see cref="Dictionary{TKey, TValue}"/> of adjacency.
+    /// </summary>
+    /// <typeparam name="TVertex">The type of every vertex.</typeparam>
+    class Graph<TVertex>
+    {
+        private readonly Dictionary<TVertex, List<TVertex>> adjacencyTable
+            = new Dictionary<TVertex, List<TVertex>>();
+
+        public Graph(TVertex[] vertices, List<TVertex[]> connections)
+        {
+            foreach (TVertex vertex in vertices)
+                adjacencyTable.Add(vertex, new List<TVertex>());
+
+            for (int i = 0; i < connections.Count; i++)
+                if (connections[i].Length == 2)
+                    AddNode(connections[i][0], connections[i][1]);
+        }
+
+        public void AddNode(TVertex origin, TVertex destination)
+        {
+            adjacencyTable[origin].Add(destination);
+            adjacencyTable[destination].Add(origin);
+        }
+
+        public bool DFS(TVertex origin, TVertex destination, List<TVertex> visited = null)
+        {
+            if (origin.Equals(destination) && adjacencyTable[origin].BinarySearch(destination) >= 0)
+                return true;
+
+            if (visited == null)
+                visited = new List<TVertex>();
+            visited.Add(origin);
+
+            foreach (TVertex connection in adjacencyTable[origin])
+                if (visited.BinarySearch(connection) < 0)
+                    if (connection.Equals(destination))
+                        return true;
+                    else
+                        return DFS(connection, destination, visited);
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            System.Text.StringBuilder builder = new System.Text.StringBuilder();
+
+            foreach (KeyValuePair<TVertex, List<TVertex>> connnection in adjacencyTable)
+            {
+                builder.Append($"{connnection.Key} => [ ");
+
+                for (int i = 0; i < connnection.Value.Count; i++)
+                    if (i < connnection.Value.Count - 1)
+                        builder.Append($"{connnection.Value[i]}, ");
+                    else builder.Append($"{connnection.Value[i]} ]\n");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GRAPHics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vertices = "KH,PK,NY,SF,LM,BGT,PAR,LON,VARNA".Split(',');
+
+            var connections = new System.Collections.Generic.List<string[]>
+            {
+                new string[2] {"KH", "PAR" },
+                new string[2] {"KH", "PK" },
+                new string[2] {"SF", "PAR" },
+                new string[2] {"LON", "PAR" },
+                new string[2] {"LON", "NY" },
+                new string[2] {"KH", "LON" },
+                new string[2] {"SF", "LON" },
+                new string[2] {"PK", "LON" },
+                new string[2] {"NY", "BGT"},
+                new string[2] {"BGT", "LM" }
+            };
+
+            Graph<string> graph = new Graph<string>(vertices, connections);
+
+            graph.AddNode("SF", "SF");
+            graph.AddNode("VARNA", "VARNA");
+
+            Console.WriteLine(graph.DFS("VARNA", "VARNA"));
+        }
+    }
+}
